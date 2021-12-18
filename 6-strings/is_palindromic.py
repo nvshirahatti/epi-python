570f7c67cd5382b98ca7694a4a810900fe891390
Aaron Nguyen <me@aaronnguyen.dev>
2021-12-18 13:17:52 -0500

    Correct space complexity and explaination

--- 6-strings/is_palindromic.py
+++ 6-strings/is_palindromic.py
@@ -1,9 +1,10 @@
-# O(n) time | O(n) space ?
+# O(n) time | O(1) space
+# Note: O(1) space because it is a for-loop, not a list comprehension
 def is_palindromic(s):
-    return all([s[i] == s[~i] for i in range(len(s) // 2)])
+    return all(s[i] == s[~i] for i in range(len(s) // 2))
 
 
 if __name__ == "__main__":
     # s = "aaron"
     s = "racecar"
-    print(is_palindromic(s))
+    print(f"{s} -> {is_palindromic(s)}")
