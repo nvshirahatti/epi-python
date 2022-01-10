65db748aa20064be462f9748cb9ca4295b9ee186
Aaron Nguyen <me@aaronnguyen.dev>
2022-01-10 13:16:40 -0500

    Use ListNode.from_list() to construct linked lists

--- 7-linked-lists/merge_two_sorted_lists.py
+++ 7-linked-lists/merge_two_sorted_lists.py
@@ -1,21 +1,8 @@
+from list_node import ListNode
+
+
 # O(m + n) time | O(1) space
 # m: L1 length; n: L2 length
-
-
-class ListNode:
-    def __init__(self, data=0, next_node=None):
-        self.data = data
-        self.next = next_node
-
-    def __str__(self) -> str:
-        sln = []
-        while self:
-            sln.append(str(self.data))
-            self = self.next
-
-        return " -> ".join(sln)
-
-
 def merge_two_sorted_lists(L1, L2):
     dummy_head = tail = ListNode()
 
@@ -32,7 +19,7 @@ def merge_two_sorted_lists(L1, L2):
 
 
 if __name__ == "__main__":
-    L1 = ListNode(2, ListNode(5, ListNode(7)))
-    L2 = ListNode(3, ListNode(11))
+    L1 = ListNode.from_list([2, 5, 7])
+    L2 = ListNode.from_list([3, 11])
     sln = merge_two_sorted_lists(L1, L2)
     print(sln)
