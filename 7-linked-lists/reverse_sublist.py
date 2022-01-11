572270f99eacd7d3d942a814ae5de2b7dbf38d76
Aaron Nguyen <me@aaronnguyen.dev>
2022-01-10 22:37:37 -0500

    Add solution in EPI

--- 7-linked-lists/reverse_sublist.py
+++ 7-linked-lists/reverse_sublist.py
@@ -3,19 +3,24 @@ from list_node import ListNode
 
 # O(n) time | O(1) space
 def reverse_sublist(L: list, start: int, finish: int):
-    # Before: 11 -> 3 -> 5 -> 7 -> 2
-    #         0     1    2    3    4
-    # After:  11 -> 7 -> 5 -> 3 -> 2
+    # Before:   11 -> 3 -> 5 -> 7 -> 2
+    # Position: 1     2    3    4    5
+    # After:    11 -> 7 -> 5 -> 3 -> 2
     dummy_head = sublist_head = ListNode(0, L)
 
-    # Go to start index
-    for _ in range(1, start):  # because of position (not index)
+    # Go to pre-start index
+    for _ in range(start - 1):  # because of position (not index)
         sublist_head = sublist_head.next
 
-    # sublist_head = 11; sublist_iter = 3; temp = 5; temp.next = 7
+    # 0 -> 11 -> 3 -> 5 -> 7 -> 2
+    # sublist_head = 11
+    # sublist_iter = 3
+    # temp = 5
+    # temp.next = 7
     sublist_iter = sublist_head.next
     for _ in range(finish - start):
         temp = sublist_iter.next
+
         sublist_iter.next = temp.next
         temp.next = sublist_head.next
         sublist_head.next = temp
