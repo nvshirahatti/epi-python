2620412d5ac63cfda8d948b06952f0ccff2cfbab
Aaron Nguyen <me@aaronnguyen.dev>
2022-02-17 08:24:59 -0500

    Change left, right to low_range, high_range

--- 14-binary-search-trees/is_binary_tree_bst.py
+++ 14-binary-search-trees/is_binary_tree_bst.py
@@ -1,11 +1,11 @@
 # O(n) time | O(h) space (aux) | h: depth/height
-def is_binary_tree_bst(root, left, right):
+def is_binary_tree_bst(root, low_range=float("-inf"), high_range=float("inf")):
     if not root:
         return True
 
-    if not (root.val < right and root.val > left):
+    if not (root.val < high_range and root.val > low_range):
         return False
 
-    return is_binary_tree_bst(root.left, left, root.val) and is_binary_tree_bst(
-        root.right, root.val, right
+    return is_binary_tree_bst(root.left, low_range, root.val) and is_binary_tree_bst(
+        root.right, root.val, high_range
     )
