1e5a77d9df6712da594056f7dcde739b1d0d0270
Aaron Nguyen <me@aaronnguyen.dev>
2022-03-16 14:03:16 -0400

    Rewrite N-Queens

--- 15-recursion/n_queens.py
+++ 15-recursion/n_queens.py
@@ -1,27 +1,35 @@
-# I am unable to understand this problem for now
-# Definitely a hard question https://leetcode.com/problems/n-queens/
 def n_queens(n):
-    def solve_n_queens(row):
-        if row == n:
-            result.append(
-                list(col_placement)
-            )  # to create a copy of list to append, instead pass ref
-            return
+    col = set()
+    pos_diag = set()  # r + c
+    neg_diag = set()  # r - c
+    board = [["."] * n for _ in range(n)]
+    res = []
 
-        for col in range(n):
-            if all(
-                abs(c - col) not in (0, row - i)
-                for i, c in enumerate(col_placement[:row])
-            ):
-                col_placement[row] = col
-                solve_n_queens(row + 1)
+    def backtrack(r):
+        if r == n:
+            res.append(["".join(row) for row in board])
 
-    result, col_placement = [], [0] * n
-    solve_n_queens(0)
-    return result
+        for c in range(n):
+            if c in col and r + c in pos_diag and r - c in neg_diag:
+                continue
+
+            col.add(c)
+            pos_diag.add(r + c)
+            neg_diag.add(r - c)
+            board[r][c] = "Q"
+
+            backtrack(r + 1)
+
+            col.remove(c)
+            pos_diag.remove(r + c)
+            neg_diag.remove(r - c)
+            board[r][c] = "."
+
+    backtrack(0)
+    return res
 
 
 if __name__ == "__main__":
     n = 4
-    sln = n_queens(n)
-    print(sln)
+    res = n_queens(n)
+    print(res)
